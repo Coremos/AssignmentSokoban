cb7a6203a3fd837599e5a18058824935a033f448
Coremos <coremos@naver.com>
2022-04-11 01:29:29 +0900

    MainActivity 수정
    Start, Edit, Exit 각각의 버튼에 클릭 이벤트를 수정했다. Exit 버튼을 누르면 다이얼로그를 출력하고 예 아니오 버튼을 통해 종료되도록 만들었다.

--- app/src/main/java/com/example/assignmentsokoban/MainActivity.java
+++ app/src/main/java/com/example/assignmentsokoban/MainActivity.java
@@ -1,16 +1,68 @@
 package com.example.assignmentsokoban;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button_start;
+    Button button_edit;
+    Button button_exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+
+        button_start = (Button)findViewById(R.id.button_start);
+        button_edit = (Button)findViewById(R.id.button_edit);
+        button_exit = (Button)findViewById(R.id.button_exit);
+
+        button_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), EditActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Alert");
+                builder.setMessage("게임을 종료하시겠습니까?");
+                builder.setNegativeButton("예", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                builder.setPositiveButton("아니오", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                builder.create().show();
+            }
+        });
     }
 }
