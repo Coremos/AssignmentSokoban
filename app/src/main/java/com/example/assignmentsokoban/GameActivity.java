b94e663e44c2f01025924190eeadc3fee5c60969
Coremos <coremos@naver.com>
2022-04-10 20:26:19 +0900

    액티비티 자바 파일 생성 및 수정
    Game, Edit, Select 액티비티의 자바 파일을 생성하고 수정했다.

---
+++ app/src/main/java/com/example/assignmentsokoban/GameActivity.java
@@ -0,0 +1,19 @@
+package com.example.assignmentsokoban;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_game);
+    }
+}
