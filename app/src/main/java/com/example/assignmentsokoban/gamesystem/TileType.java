08d991abd614ea62fe4a961381eb8622068d494d
Coremos <coremos@naver.com>
2022-04-21 23:44:16 +0900

    TileType 클래스 생성
    리소스를 사용하기 위해 각각의 리소스 이름에 해당하는 상수 값을 정적 변수로 저장했다.

---
+++ app/src/main/java/com/example/assignmentsokoban/gamesystem/TileType.java
@@ -0,0 +1,11 @@
+package com.example.assignmentsokoban.gamesystem;
+
+public class TileType {
+    static final int Ground = 0;
+    static final int Tree = 1;
+    static final int Chick = 2;
+    static final int Pond = 3;
+    static final int ChickPond = 4;
+    static final int Duck = 5;
+    static final int Count = 6;
+}
