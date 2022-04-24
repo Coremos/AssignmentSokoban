f418c1da29ed79a2132ead65a0a506e71e33d630
Coremos <coremos@naver.com>
2022-04-24 18:31:27 +0900

    GameView 클래스 수정
    비트맵을 불러온 다음 2차원 배열의 맵에 해당하는 비트맵들을 화면에 출력하도록 만들었다.

--- app/src/main/java/com/example/assignmentsokoban/gamesystem/GameView.java
+++ app/src/main/java/com/example/assignmentsokoban/gamesystem/GameView.java
@@ -1,6 +1,9 @@
 package com.example.assignmentsokoban.gamesystem;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -13,13 +16,23 @@ import com.example.assignmentsokoban.GameActivity;
 
 public class GameView extends ParentView {
     private static final int UNIT_SIZE = 32;
-    private static final int GAMEVIEW_WIDTH = UNIT_SIZE * 32;
-    private static final int GAMEVIEW_HEIGHT = UNIT_SIZE * 18;
+    private static final int GAMEVIEW_WIDTH = UNIT_SIZE * 16;
+    private static final int GAMEVIEW_HEIGHT = UNIT_SIZE * 9;
+    private static final int STAGE_PIVOT_X = (int)(UNIT_SIZE * 3);
+    private static final int STAGE_PIVOT_Y = (int)(UNIT_SIZE * 0);
+    private static final int MAP_WIDTH = 9;
+    private static final int MAP_HEIGHT = 9;
 
     private GameActivity _gameActivity;
     private MainThread _mainThread;
     private ScreenGetter _screenGetter;
+    private ResourceInfo _resourceInfo;
+    private Bitmap[] _bitmaps;
     private boolean _canDraw;
+    private Resources _resources;
+
+    private int playerX = 3;
+    private int playerY = 3;
 
     public GameView(Context context, AttributeSet attributeSet)
     {
@@ -27,12 +40,6 @@ public class GameView extends ParentView {
         getHolder().addCallback(this);
         _mainThread = new MainThread(getHolder(), this);
         _canDraw = false;
-        loadResources();
-    }
-
-    public void loadResources()
-    {
-        //Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.);
     }
 
     public void initialize(GameActivity gameActivity, int screenWidth, int screenHeight)
@@ -41,6 +48,16 @@ public class GameView extends ParentView {
         _screenGetter = new ScreenGetter(screenWidth, screenHeight);
         _screenGetter.setGameViewSize(GAMEVIEW_WIDTH, GAMEVIEW_HEIGHT);
         _canDraw = true;
+
+        _resourceInfo = new ResourceInfo();
+        _resources = getResources();
+
+        _bitmaps = new Bitmap[TileType.Count];
+        for (int i = 0; i < TileType.Count; i++)
+        {
+            Bitmap bitmap = BitmapFactory.decodeResource(_resources, _resourceInfo.Resources[i]);
+            _bitmaps[i] = Bitmap.createScaledBitmap(bitmap, _screenGetter.getX(UNIT_SIZE), _screenGetter.getY(UNIT_SIZE), false);
+        }
     }
 
     private void drawDebug(Canvas canvas)
@@ -52,11 +69,54 @@ public class GameView extends ParentView {
         canvas.drawRect(_screenGetter.getX(GAMEVIEW_WIDTH - UNIT_SIZE), _screenGetter.getY(GAMEVIEW_HEIGHT - UNIT_SIZE), _screenGetter.getX(GAMEVIEW_WIDTH), _screenGetter.getY(GAMEVIEW_HEIGHT), backPaint);
     }
 
+    private void drawMap(Canvas canvas)
+    {
+        int map[][] = new int[MAP_WIDTH][MAP_HEIGHT];
+        int scaledPivotX = _screenGetter.getX(STAGE_PIVOT_X);
+        int scaledPivotY = _screenGetter.getY(STAGE_PIVOT_Y);
+        int scaledUnitX = _screenGetter.getX(UNIT_SIZE);
+        int scaledUnitY = _screenGetter.getY(UNIT_SIZE);
+        Paint pixelPaint = new Paint();
+        pixelPaint.setAntiAlias(false);
+        pixelPaint.setFilterBitmap(false);
+        Log.w("Paint", "isAntiAlias " + pixelPaint.isAntiAlias());
+        for (int x = 0; x < MAP_WIDTH; x++)
+        {
+            for (int y = 0; y < MAP_HEIGHT; y++)
+            {
+                map[x][y] = TileType.Ground;
+                if (x == 0 || x == MAP_WIDTH - 1 || y == 0 || y == MAP_HEIGHT - 1)
+                    map[x][y] = TileType.Tree;
+            }
+        }
+
+        map[1][1] = TileType.Pond;
+        map[2][1] = TileType.Pond;
+        map[3][1] = TileType.Pond;
+
+        map[1][2] = TileType.Chick;
+        map[2][2] = TileType.Chick;
+        map[3][2] = TileType.Chick;
+
+        for (int x = 0; x < MAP_WIDTH; x++)
+        {
+            for (int y = 0; y < MAP_HEIGHT; y++)
+            {
+                canvas.drawBitmap(_bitmaps[map[x][y]], scaledPivotX + scaledUnitX * x,
+                        scaledPivotY + scaledUnitY * y, pixelPaint);
+            }
+        }
+
+        canvas.drawBitmap(_bitmaps[TileType.Duck], scaledPivotX + scaledUnitX * playerX,
+                scaledPivotY + scaledUnitY * playerY, pixelPaint);
+    }
+
     @Override
     protected void onDraw(Canvas canvas)
     {
         if (!_canDraw) return;
-        drawDebug(canvas);
+        //drawDebug(canvas);
+        drawMap(canvas);
     }
 
     public boolean onTouchEvent(MotionEvent event) { return true; }
