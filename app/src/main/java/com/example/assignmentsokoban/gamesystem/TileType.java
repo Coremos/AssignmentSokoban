5457624062faa67f7599e97adc82e3d30748b9d1
Coremos <coremos@naver.com>
2022-05-09 23:03:08 +0900

    TileType 클래스 속성 접근 한정자 수정
    접근 한정자를 모두 public으로 변경했다.

--- app/src/main/java/com/example/assignmentsokoban/gamesystem/TileType.java
+++ app/src/main/java/com/example/assignmentsokoban/gamesystem/TileType.java
@@ -1,11 +1,11 @@
 package com.example.assignmentsokoban.gamesystem;
 
 public class TileType {
-    static final int Ground = 0;
-    static final int Tree = 1;
-    static final int Chick = 2;
-    static final int Pond = 3;
-    static final int ChickPond = 4;
-    static final int Duck = 5;
-    static final int Count = 6;
+    public static final int Ground = 0;
+    public static final int Tree = 1;
+    public static final int Chick = 2;
+    public static final int Pond = 3;
+    public static final int ChickPond = 4;
+    public static final int Duck = 5;
+    public static final int Count = 6;
 }
