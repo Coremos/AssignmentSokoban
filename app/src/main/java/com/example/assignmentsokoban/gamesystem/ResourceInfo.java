814dbd51094fac38fa823539f05cc4792dae9a9c
Coremos <coremos@naver.com>
2022-05-11 23:53:25 +0900

    UI 리소스 추가
    방향키 버튼과 돌아가기 버튼의 리소스를 추가했다.
    해당 사항을 수정하기 위해 ResourceInfo 클래스의 속성도 수정했다.

--- app/src/main/java/com/example/assignmentsokoban/gamesystem/ResourceInfo.java
+++ app/src/main/java/com/example/assignmentsokoban/gamesystem/ResourceInfo.java
@@ -3,16 +3,22 @@ package com.example.assignmentsokoban.gamesystem;
 import com.example.assignmentsokoban.R;
 
 public class ResourceInfo {
+    public static final int Pad = 7;
+    public static final int BackButton = 8;
+    public static final int Count = 9;
+
     public int[] Resources;
 
     public ResourceInfo()
     {
-        Resources = new int[TileType.Count];
+        Resources = new int[ResourceInfo.Count];
         Resources[TileType.Ground] = R.drawable.ground;
         Resources[TileType.Tree] = R.drawable.tree;
         Resources[TileType.Chick] = R.drawable.chick;
         Resources[TileType.Pond] = R.drawable.pond;
         Resources[TileType.ChickPond] = R.drawable.chickpond;
         Resources[TileType.Duck] = R.drawable.duck;
+        Resources[ResourceInfo.Pad] = R.drawable.pad;
+        Resources[ResourceInfo.BackButton] = R.drawable.backbutton;
     }
 }
