1847335ed2d3e2249e0c78e4e9359888e72e1b78
Coremos <coremos@naver.com>
2022-05-12 23:01:05 +0900

    MainActivity 클래스 수정
    생략 가능한 자료형 변환을 생략하고, 거추장스러운 메서드를 람다식으로 수정했다.

--- app/src/main/java/com/example/assignmentsokoban/MainActivity.java
+++ app/src/main/java/com/example/assignmentsokoban/MainActivity.java
@@ -3,12 +3,8 @@ package com.example.assignmentsokoban;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,49 +16,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
 
-        button_start = (Button)findViewById(R.id.button_start);
-        button_edit = (Button)findViewById(R.id.button_edit);
-        button_exit = (Button)findViewById(R.id.button_exit);
+        button_start = findViewById(R.id.button_start);
+        button_edit = findViewById(R.id.button_edit);
+        button_exit = findViewById(R.id.button_exit);
 
-        button_start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), GameActivity.class);
-                startActivity(intent);
-            }
+        button_start.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+            startActivity(intent);
         });
 
-        button_edit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), EditActivity.class);
-                startActivity(intent);
-            }
+        button_edit.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), EditActivity.class);
+            startActivity(intent);
         });
 
-        button_exit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Alert");
-                builder.setMessage("게임을 종료하시겠습니까?");
-                builder.setNegativeButton("예", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        finish();
-                    }
-                });
-                builder.setPositiveButton("아니오", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.cancel();
-                    }
-                });
-                builder.create().show();
-            }
+        button_exit.setOnClickListener(view -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Alert");
+            builder.setMessage("게임을 종료하시겠습니까?");
+            builder.setNegativeButton("예", (dialogInterface, i) -> finish());
+            builder.setPositiveButton("아니오", (dialogInterface, i) -> dialogInterface.cancel());
+            builder.create().show();
         });
     }
 }
