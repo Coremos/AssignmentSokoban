3bfb7de104095899b3e21f21bc208d3031c55bc6
Coremos <coremos@naver.com>
2022-05-15 23:19:12 +0900

    GameActivity 클래스 수정
    ActionBar를 숨기도록 수정하고, 생략 가능한 자료형 변환을 제거했다.

--- app/src/main/java/com/example/assignmentsokoban/GameActivity.java
+++ app/src/main/java/com/example/assignmentsokoban/GameActivity.java
@@ -5,21 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.Display;
-import android.view.WindowManager;
 
 import com.example.assignmentsokoban.gamesystem.GameView;
 
 public class GameActivity extends AppCompatActivity {
-
     GameView gameView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_game);
 
-        gameView = (GameView)findViewById(R.id.view_game);
+        gameView = findViewById(R.id.view_game);
         Display display = getWindowManager().getDefaultDisplay();
         int screenWidth = display.getWidth();
         int screenHeight = display.getHeight();
